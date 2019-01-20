29e7e03524b8544f5d4fda5107f238903b7f137d
Jeff Spingeld <jeff.spingeld@gmail.com>
2019-01-20 14:11:11 -0800

    Add redux-persist

--- src/redux-files/configureStore.js
+++ src/redux-files/configureStore.js
@@ -1,8 +1,18 @@
 import { createStore, combineReducers } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import hardSet from 'redux-persist/lib/stateReconciler/hardSet'
+//
 import ui from 'reducers/ui';
 import items from 'reducers/items';
 import categoryOrder from 'reducers/categoryOrder';
 import tripTypes from 'reducers/tripTypes';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    stateReconciler: hardSet
+};
 
 const configureStore = () => {
     const rootReducer = combineReducers({
@@ -11,8 +21,10 @@ const configureStore = () => {
         tripTypes,
         ui
     });
-    const store = createStore(rootReducer);
-    return store;
+    const persistedReducer = persistReducer(persistConfig, rootReducer)
+    const store = createStore(persistedReducer);
+    const persistor = persistStore(store);
+    return { store, persistor };
 };
 
 export default configureStore;
