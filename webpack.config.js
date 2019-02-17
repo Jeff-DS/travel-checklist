c8a053acc7e122070438f80e1b6296fd30d25cf4
Jeff Spingeld <jeff.spingeld@gmail.com>
2019-02-16 19:12:13 -0800

    move path aliases to tsconfig so VS code recognizes them

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,65 @@
 const path = require('path');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV === 'test') {
-  require('dotenv').config({ path: '.env.test' });
+    require('dotenv').config({ path: '.env.test' });
 } else if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config({ path: '.env.development' });
+    require('dotenv').config({ path: '.env.development' });
 }
 
-module.exports = (env) => {
-  const isProduction = env === 'production';
-  const CSSExtract = new MiniCssExtractPlugin('styles.css');
+module.exports = env => {
+    const isProduction = env === 'production';
+    const CSSExtract = new MiniCssExtractPlugin('styles.css');
 
-  return {
-    entry: ['@babel/polyfill', './src/index.js'],
-    output: {
-      path: path.join(__dirname, 'public', 'dist'),
-      filename: 'bundle.js'
-    },
-    resolve: {
-      alias: {
-        src: path.resolve(__dirname, 'src'),
-        styles: path.resolve(__dirname, 'src/styles'),
-        components: path.resolve(__dirname, 'src/components'),
-        actions: path.resolve(__dirname, 'src/redux-files/actions'),
-        actionTypes$: path.resolve(__dirname, 'src/redux-files/actionTypes.js'),
-        reducers: path.resolve(__dirname, 'src/redux-files/reducers'),
-        selectors$: path.resolve(__dirname, 'src/redux-files/selectors.js'),
-        utils$: path.resolve(__dirname, 'src/utils.js')
-      }
-    },
-    module: {
-      rules: [
-        {
-          loader: 'babel-loader',
-          test: /\.js$/,
-          exclude: /node_modules/
+    return {
+        entry: ['@babel/polyfill', './src/index.js'],
+        output: {
+            path: path.join(__dirname, 'public', 'dist'),
+            filename: 'bundle.js'
         },
-        {
-          test: /\.css$/,
-          use: [
-            'style-loader',
-            {
-              loader: 'css-loader',
-              options: {
-                modules: true
-              }
-            }
-          ]
+        resolve: {
+            plugins: [
+                new TsconfigPathsPlugin({
+                    extensions: ['.js', '.jsx', '.ts', 'tsx']
+                })
+            ]
+        },
+        module: {
+            rules: [
+                {
+                    loader: 'babel-loader',
+                    test: /\.js$/,
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        'style-loader',
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: '[name].css',
+                chunkFilename: '[id].css'
+            })
+        ],
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true,
+            publicPath: '/dist/'
         }
-      ]
-    },
-    plugins: [
-      new MiniCssExtractPlugin({
-        filename: "[name].css",
-        chunkFilename: "[id].css"
-      })
-    ],
-    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
-    devServer: {
-      contentBase: path.join(__dirname, 'public'),
-      historyApiFallback: true,
-      publicPath: '/dist/'
-    }
-  };
+    };
 };
